00b549fadaae885c37b19f7f13cca614b3741f80
Gustavo Velazquez Moctezuma <gustavo@softwaredevtools.com>
2020-06-11 23:09:39 -0500

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 110,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+};
